1c9179c2f479f474ed653f9e5ce77085b84cbe3e
Chinedu Ofor <chineduofor@Chinedu-Ofor.local>
2017-05-23 15:10:21 +0100

    feature(notifications): add notifications[Delivers #145919407]

--- src/source/MessageSource.js
+++ src/source/MessageSource.js
@@ -3,6 +3,8 @@ require('firebase/auth');
 require('firebase/database');
 
 let firebase = null;
+let messageSent = null;
+let initialLoad = true;
 
 const MessageSource = {
 
@@ -15,6 +17,7 @@ const MessageSource = {
         // console.log(JSON.parse(localStorage.getItem('state')))
         const user = JSON.parse(localStorage.getItem('state'));
         const userPic = user.photoURL;
+        messageSent = state.user.uid;
         firebase.database().ref('/messages/' + state.selectedChannel.name).push({
           'message': state.message,
           'date': new Date().toUTCString(),
@@ -22,6 +25,7 @@ const MessageSource = {
           'userId': state.user.uid,
           'profilePic': userPic
         });
+        // alert(`I ${state.user.uid} am sending an awesome message to every mnember in the group :)`);
         resolve();
       });
     },
@@ -34,12 +38,31 @@ const MessageSource = {
       if (firebase) {
         firebase = null;
       }
+      const user = JSON.parse(localStorage.getItem('state'));
       firebase = require('firebase/app');
       return new Promise((resolve, reject) => {
         firebase.database().ref('/messages/' + state.selectedChannel.name).on('value', (dataSnapshot) => {
           const messages = dataSnapshot.val();
+          if (!messageSent && !initialLoad) {
+            if(window.Notification && Notification.permission !== "denied") {
+              // alert();
+              console.log('bla')
+              Notification.requestPermission(function(status) {  // status is "granted", if accepted by user
+                let n = new Notification('PostIt', { 
+                  body: 'You have a new notification!',
+                  icon: '../../public/images/PostIt.jpg' // optional
+                }); 
+              });
+              setTimeout(n.close.bind(n), 2000);
+            }
+            messageSent = null;
+          }
           resolve(messages);
+          // alert('i am called upon :) :)');
+          initialLoad = false;
+          // messageSent = null;
           firebase.database().ref('/messages/' + state.selectedChannel.name).on('child_added', (msg) => {
+            // alert();
             const msgVal = msg.val();
             msgVal.key = msg.key;
             Actions.messageReceived(msgVal);

1871cc89b5e8ab8d5d43d2d74f4e9f2f0e9fe07a
Chinedu Ofor <chineduofor@Chinedu-Ofor.local>
2017-05-25 10:35:55 +0100

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -145,10 +145,6 @@ class Login extends React.Component {
             type="submit"
             style={{ margin: 12, }}
           />
-          <button type="submit"
-            name="submit">
-            <i className="fa fa-google-plus" aria-hidden="true"></i>
-          </button>
           <RaisedButton
             id="loginWithGoogle"
             onClick={this.loginWithGoogle}
